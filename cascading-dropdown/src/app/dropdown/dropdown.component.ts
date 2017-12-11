77cb12184e132052059ffc7a528d54dfb75b58d5
Anshul Dongre <anshuld@nitorinfotech.net>
2017-12-11 19:37:51 +0530

    added basic logic for creation of dropdown

--- cascading-dropdown/src/app/dropdown/dropdown.component.ts
+++ cascading-dropdown/src/app/dropdown/dropdown.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core/src/metadata/directives';
+import { IDropdown } from '../interfaces/IDropdown';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-dropdown',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DropdownComponent implements OnInit {
 
+  @Input('values') public values: IDropdown = undefined;
+
+  @Input() node = {
+    'name': 'root',
+    'type': 'root',
+    'children':
+      [
+        {
+          'type': 'Continents',
+          'name': 'Asia',
+          'children': [
+            {
+              'type': 'Country',
+              'name': 'India',
+              'children': [
+                {
+                  'type': 'State',
+                  'name': 'MH',
+                  'children': [
+                    {
+                      'type': 'City',
+                      'name': 'Pune',
+                      'children': null
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
+  };
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
+  iterateValues = (data: IDropdown) => {
+    _.forIn(data, function (element, index) {
+
+    });
+  }
 }
---
+++ cascading-dropdown/src/app/interfaces/IDropdown.ts
@@ -0,0 +1,5 @@
+export interface IDropdown {
+    name: string;
+    type: string;
+    value?: any;
+}
